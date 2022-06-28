781250888b5525ce921058ebca62784319bc52b0
moham <mohammad.din63@gmail.com>
2022-06-28 00:06:33 -0400

    Initial commit

---
+++ src/Day12June25/Task01.java
@@ -0,0 +1,37 @@
+package Day12June25;
+
+
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+        // اولیاب
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int counter = 0;
+
+        // A find the divisible number
+        for (int number = num1; number < num2; number++) {
+            boolean isPrime = true;
+            int numberOfDivisor = 0;
+
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) { // this number is divisible by i number
+                    isPrime = false;
+                }
+            }
+            if (isPrime) {
+                System.out.println(number);
+                counter++;
+
+            } else {
+
+            }
+        }
+        if(counter == 0){
+            System.out.println("0");
+        }
+
+    }
+}
---
+++ src/Day12June25/Task03.java
@@ -0,0 +1,4 @@
+package Day12June25;
+
+public class Task03 {
+}
